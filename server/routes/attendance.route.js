6874180f57e7e2ee86f5faea6b7dbdecfac8a96d
Anurag 729 <anuragran07@gmail.com>
2025-06-09 17:54:31 +0530

    Made further changes

--- server/routes/attendance.route.js
+++ server/routes/attendance.route.js
@@ -5,6 +5,7 @@ import {
   endSession,
   storeRecords,
   getActiveattendance,
+  redisConnect,
 } from "../controllers/attendance.controller.js";
 
 const attendanceRouter = Router();
@@ -14,5 +15,6 @@ attendanceRouter.route("/getMarked").post(getMarked);
 attendanceRouter.route("/endSession").post(endSession);
 attendanceRouter.route("/storeRecords").post(storeRecords);
 attendanceRouter.route("/getActiveAttendance").post(getActiveattendance);
+attendanceRouter.route("/connect").get(redisConnect);
 
 export default attendanceRouter;
